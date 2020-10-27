5936f98456c7b54999352fed6be87111c6c8fb4b
James Spece <specej@gmail.com>
2020-10-26 21:50:25 -0400

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by James Spece on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
